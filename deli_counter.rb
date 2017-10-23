b8276542c11a72ea86d25d9f519738fa1ca3e732
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-23 19:40:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,13 @@ def take_a_number(arr,name)
   arr << name
   puts "Welcome, #{name}. You are number #{arr.size} in line."
 end
+
+def now_serving(arr)
+  if arr.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "#{arr.first}, you are up!"
+    arr.shift()
+  end
+  
+end
