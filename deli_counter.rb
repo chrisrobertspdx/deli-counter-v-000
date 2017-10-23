dfdc5e44b2f7de1987d6889602e95041e77acbab
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-23 19:27:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,9 @@ def line(deli)
   if deli.size == 0
     return "The line is empty."
   else
-    return deli.each_with_index.collect do |e,i|
+    result = deli.each_with_index.collect do |e,i|
       "#{i+1}. #{e}."
-    end.join(' ')
+    end
+    return result.join(' ')
+  end
 end
