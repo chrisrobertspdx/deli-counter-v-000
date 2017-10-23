770d361b5df34cbdf6ffa8b6c5a6675b6afec75e
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-23 19:16:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 katz_deli = []
+
+def line(deli) {
+  case deli.size
+  when 1
+    return deli.first
+  when 2
+    return "#{deli.first} and #{deli.last}"
+  else
+    return deli.take(deli.size-1).join(", ") + ", and #{deli.last}"
+  end
+end
+}
