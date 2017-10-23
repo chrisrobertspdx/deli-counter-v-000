9c3bf766bfe3fe54297fff8031fa15bdf09cde57
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-23 19:36:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 
 def take_a_number(arr,name)
   arr << name
-  return "Welcome, #{name}. You are number #{arr.size} in line."
+  puts "Welcome, #{name}. You are number #{arr.size} in line."
 end
